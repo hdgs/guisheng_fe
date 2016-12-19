6cbfd043ff7181de4497a6e9dad6ba702a6192c3
Amanda111 <631551773@qq.com>
2016-12-19 23:42:57 +0800

    profile list

--- src/components/profile/index.vue
+++ src/components/profile/index.vue
@@ -16,6 +16,37 @@
         	</div>
         	<div :class="$style.intro">个人介绍：这是作者的个人介绍。这个人好懒啊，他什么也没写但还是要凑字数。也许两行不够介绍，那就三行……</div>
         </div>
+        <div :class="$style.list">
+        	<div :class="$style.col">
+    			<svg viewBox="0 0 200 200" :class="$style.largeimg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#work"></use></svg>
+        		<div :class="$style.text">
+        			<a>我的作品</a>
+        			<a :class="$style.arrow">></a>
+        		</div>
+        	</div>
+        	<div :class="$style.col">
+    			<svg viewBox="0 0 200 200" :class="$style.largeimg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#work"></use></svg>
+        		<div :class="$style.text">
+        			<a>我的作品</a>
+        			<a :class="$style.arrow">></a>
+        		</div>
+        	</div>
+        	<div :class="$style.col">
+    			<svg viewBox="0 0 200 200" :class="$style.largeimg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#work"></use></svg>
+        		<div :class="$style.text">
+        			<a>我的作品</a>
+        			<a :class="$style.arrow">></a>
+        		</div>
+        	</div>
+        	<div :class="$style.col">
+    			<svg viewBox="0 0 200 200" :class="$style.largeimg"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#work"></use></svg>
+        		<div :class="$style.text">
+        			<a>我的作品</a>
+        			<a :class="$style.arrow">></a>
+        		</div>
+        	</div>
+        </div>
+        <button :class="$style.signout">登出</button>
     </div>
 </template>
 <script>
@@ -94,5 +125,45 @@ html,body{
 .sign{
 	margin-top: 15px;
 	text-indent: 20px;
+	color: $orange;
+}
+.list{
+	width: 100%;
+}
+.col{
+	height: 50px;
+	font-size: 0;
+}
+.largeimg{
+	width: 30px;
+   	fill: $orange;
+    vertical-align: middle;	
+    margin: 0 16px;
+}
+.text{
+	display: inline-block;
+	height: 50px;
+	line-height: 50px;
+	font-size: 16px;
+	vertical-align: middle;
+	color:$black;
+	width: calc(100% - 62px);
+	box-sizing: border-box;
+	border-bottom: 1px solid #e6e6e6;
+}
+.arrow{
+	float: right;
+	margin-right: 26px;
+	transform: scale(1, 2);
+	overflow: hidden;
+	font-size: 16px;
+	color: #8a8a8a;
+}
+.signout{
+	width: 180px;
+	height: 36px;
+	box-sizing: border-box;
+	border: 1px $orange solid;
+	background-color: $white;
 }
 </style>
