10e1de21723eb1ad76d8dbb1e55dddd126a821cb
Elegenthus <996886231@qq.com>
2017-02-08 23:58:50 +0800

    update profile

--- src/components/profile/index.vue
+++ src/components/profile/index.vue
@@ -45,21 +45,21 @@
                 <svg viewBox="0 0 200 200" :class="$style.largeimg">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#collection"></use>
                 </svg>
-                <div :class="$style.text">
+                <div :class="$style.text" v-on:click="showCollection">
                     <a>我的收藏</a>
                     <a :class="$style.arrow">></a>
                 </div>
             </div>
-            <div :class="$style.col">
+            <div :class="$style.col" v-on:click="showMyWorks">
                 <svg viewBox="0 0 200 200" :class="$style.largeimg">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#work"></use>
                 </svg>
-                <div :class="$style.text">
+                <div :class="$style.text" >
                     <a>他的作品</a>
                     <a :class="$style.arrow">></a>
                 </div>
             </div>
-            <div :class="$style.col">
+            <div :class="$style.col" v-on:click = "showMySuggest">
                 <svg viewBox="0 0 200 200" :class="$style.largeimg">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#feedback"></use>
                 </svg>
@@ -78,17 +78,51 @@
                 </div>
             </div>
         </div>
-        <button :class="$style.signout">登出</button>
+        <button :class="$style.signout" v-on:click = "showReturn">登出</button>
+        <div v-show = "returnIt" :class = "$style.suggestMask">
+                <div :class = "$style.returnCard">
+                    <div :class= "$style.returnContent">退出登录后，将不能发表评论和收藏内容。确认退出？</div>
+                    <div :class= "$style.returnButton">退出</div>
+                    <div :class= "$style.returnButton" v-on:click = "quit">取消</div>
+                </div>
+            </div>
         <div v-show="showWorks" :class="$style.commentPage">
             <div :class="$style.titleBox">
                 <svg viewBox="0 0 200 200" :class="$style.imgBack" v-on:click="closeComment">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#back"></use>
                 </svg>
-                <div :class="$style.commentTitle">我的作品</div>
+                <div :class="$style.commentTitle">{{this.title}}</div>
             </div>
             <item :item="item" v-for="item in list"></item>
             <div :class = "$style.tip">  Σ( ° △ °|||)已经没有了</div>
         </div>
+        <div v-show="showSuggest" :class="$style.suggestPage">
+            <div :class="$style.titleBox">
+                <svg viewBox="0 0 200 200" :class="$style.imgBack" v-on:click="closeComment">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#back"></use>
+                </svg>
+                <div :class="$style.commentTitle">意见反馈</div>
+            </div>
+            <div :class = "$style.line"></div>
+            <div :class = "$style.title_s">您的意见：</div>
+            <textarea type="text" :class = "$style.input" placeholder="输入您的反馈详情" v-model = "suggest"></textarea>
+            <div :class = "$style.title_s">联系方式：</div>
+            <textarea type="text" :class = "$style.inputInfo" placeholder="手机号码/QQ/邮箱" v-model = "suggestInfo"></textarea>
+            <button :class="$style.signout" v-on:click = "submitSuggest">提交</button>
+            <div v-show = "showSuccess" :class = "$style.suggestMask">
+                <div :class = "$style.successCard">提交成功啦 ~</div>
+            </div>
+        </div>
+
+        <!-- <div v-show = "changeMessage" :class="$style.suggestPage">
+            <div :class="$style.titleBox">
+                <svg viewBox="0 0 200 200" :class="$style.imgBack" v-on:click="closeComment">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#back"></use>
+                </svg>
+                <div :class="$style.commentTitle">个人信息</div>
+            </div>
+        </div> -->
+
     </div>
 </template>
 <script>
@@ -100,7 +134,13 @@ export default {
                 foo: 1,
                 profile: {},
                 showWorks: false,
-                list: []
+                list: [],
+                title:"",
+                showSuggest:false,
+                showSuccess:false,
+                suggest:"",
+                suggestInfo:"",
+                returnIt:false
             }
         },
         components: {
@@ -115,6 +155,9 @@ export default {
             })
         },
         methods: {
+            showReturn(){
+                this.returnIt = true
+            },
             showMyWorks() {
                 this.showWorks = true
                 fetch('/api/v1.0/profile/works').then( res => {
@@ -122,10 +165,68 @@ export default {
                 }).then( value => {
                     this.list = value
                     console.log(value)
+                    this.title = "我的作品"
+                })
+            },
+            quit(){
+                this.returnIt = false
+            },
+            showCollection(){
+                this.showWorks = true
+                fetch('/api/v1.0/profile/collection').then( res => {
+                    return res.json()
+                }).then( value => {
+                    this.list = value
+                    console.log(value)
+                    this.title = "我的收藏"
                 })
             },
             closeComment() {
-                this.showWorks = false
+                if(this.showWorks)
+                    this.showWorks = false
+                if(this.showSuggest){
+                    this.showSuggest = false
+                    this.suggestion = ""
+                    this.suggestInfo = ""
+                }
+            },
+            showMySuggest(){
+                this.showSuggest = true
+            },
+            closeIt(){
+                this.showSuggest = false
+                this.showSuccess = false
+                console.log(this.showSuccess)
+            },
+            submitSuggest(){
+                console.log(this.suggestInfo,this.suggest)
+                if(!this.suggest|| !this.suggestInfo) return
+                fetch('/api/v1.0/profile/suggestion/', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            suggestion: this.suggestion,
+                            contact_information: this.suggestInfo
+                        })
+                    })
+                    .then(res => {
+                        return res.json()
+                    }).then(value => {
+                        console.log(value,this.showSuccess)
+                        this.showSuccess = true
+                        this.suggestion = ""
+                        console.log(this.showSuccess)
+                        this.suggestInfo = ""
+                        setTimeout( () => {
+                            this.showSuggest = false
+                            this.showSuccess = false
+                            console.log(this.showSuccess)
+                        },2000)
+
+                    })
             }
         }
 }
@@ -138,7 +239,73 @@ body {
     width: 100%;
     height: 100%;
 }
-
+textarea{
+    outline: none;
+    border: none;
+}
+.returnCard{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 250px;
+    height: 90px;
+    background-color: $white;
+    color:$black;
+    padding: 15px;
+    border-radius: 2px;
+    transform: translate(-50%,-50%);
+}
+.returnButton{
+    float: right;
+    margin-left: 25px;
+    color: $orange;
+    cursor: pointer;
+    margin-top: 16px;
+}
+.line{
+    width: 100%;
+    height: 0.5px;
+    background-color: $grey;
+}
+.suggestMask{
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: rgba(51,51,51,0.85);
+}
+.successCard{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 173px;
+    height: 63px;
+    background-color: $white;
+    line-height: 63px;
+    text-align: center;
+    color:$orange;
+    border-radius: 2px;
+    transform: translate(-50%,-50%);
+}
+.title_s{
+    font-size: 15px;
+    margin-left: 15px;
+    margin-top: 15px;
+    color: $orange;
+}
+.input{
+    width: 100%;
+    height: 110px;
+    font-size: 15px;
+    padding: 15px;
+    border-bottom: 0.50px solid $grey;
+}
+.inputInfo{
+    width: 100%;
+    font-size: 15px;
+    padding: 15px;
+    border-bottom: 0.50px solid $grey;
+}
 .profile {
     width: 100%;
     height: 100%;
@@ -279,6 +446,17 @@ body {
     z-index: 4;
 }
 
+.suggestPage {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background-color: $white;
+    z-index: 4;
+    font-family: '黑体-简';
+}
+
 .imgBack {
     composes: horizon from 'sass-loader!../../scss/utility.scss';
     width: 19px;
@@ -294,9 +472,9 @@ body {
 }
 
 .titleBox {
-    height: 52px;
+    height: 54px;
     width: 100%;
-    line-height: 52px;
+    line-height: 54px;
     color: $black;
     background-color: $white;
     composes: space from 'sass-loader!../../scss/utility.scss';
