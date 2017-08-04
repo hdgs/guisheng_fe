204643cb855fc1cc9e2caa566ab6e9031bc6651e
Elegenthus <996886231@qq.com>
2017-08-04 13:55:30 +0800

    another try

--- src/components/second/commentBox.vue
+++ src/components/second/commentBox.vue
@@ -42,9 +42,6 @@ export default {
                 }
             }
         },
-        mounted(){
-            console.log(this.comment)
-        },
         methods: {
             addCommentLike: function (comment, index) {
                 if (this.greatComment) return
