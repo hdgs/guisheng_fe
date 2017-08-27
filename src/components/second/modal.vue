a3a1eeacddda07be3a3776145c2fa1e11b5f970c
Elegenthus <996886231@qq.com>
2017-08-27 10:39:26 +0800

    change url

--- src/components/second/modal.vue
+++ src/components/second/modal.vue
@@ -17,7 +17,7 @@ export default {
         login() {
             this.quit()
             Cookie.setCookie('url', window.location.href)
-            window.location = "https://user.muxixyz.com?landing=119.23.35.1:8777/landing"
+            window.location = "https://user.muxixyz.com?landing=gs.muxixyz.com/landing"
             // window.location = "https://user.muxixyz.com?landing=localhost:3000/landing"
         }
     }
